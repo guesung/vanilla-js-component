6c2e563d32a7da3e9f275f9612f5a292561b19c6
guesung <gueit214@naver.com>
2025-03-10 09:45:32 +0900

    feat: Vanilla JavaScript로 요소 주입

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+document.querySelector("#app").innerHTML = `
+  <main id="foods">
+  </main>
+`;
 
-setupCounter(document.querySelector('#counter'))
+document.querySelector("#foods").innerHTML = `
+  <h1>내가 가장 좋아하는 음식들</h1>
+  <ul>
+    <li>삼겹살</li>
+    <li>감자탕</li>
+    <li>치킨</li>
+  </ul>
+`;

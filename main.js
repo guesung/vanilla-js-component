7111eded2f35cc40041ff492e5a45be542848604
guesung <gueit214@naver.com>
2025-03-10 09:57:55 +0900

    feat: 음식 추가하는 로직

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
 import "./style.css";
 
-document.querySelector("#app").innerHTML = `
-  <main id="foods">
-  </main>
-`;
+function appendMain() {
+  document.querySelector("#app").innerHTML = `
+    <main id="foods">
+    </main>
+  `;
+}
 
-document.querySelector("#foods").innerHTML = `
-  <h1>내가 가장 좋아하는 음식들</h1>
-  <ul>
-    <li>삼겹살</li>
-    <li>감자탕</li>
-    <li>치킨</li>
-  </ul>
-`;
+const foods = ["삼겹살", "감자탕", "치킨"];
+
+function appendFoods(foods) {
+  document.querySelector("#foods").innerHTML = `
+    <h1>내가 가장 좋아하는 음식들</h1>
+    <ul>
+      ${foods.map((food) => `<li>${food}</li>`).join("")}
+    </ul>
+    <form>
+      <input id="add-food" />
+      <button>음식 추가</button>
+    </form>
+  `;
+
+  document
+    .querySelector("#foods")
+    .querySelector("form")
+    .addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      const addFoodInputValue = document.querySelector("#add-food").value;
+      foods.push(addFoodInputValue); // 피자
+
+      console.log(foods); // ['삼겹살', '감자탕', '치킨', '피자']
+    });
+}
+
+appendMain();
+appendFoods(foods);
